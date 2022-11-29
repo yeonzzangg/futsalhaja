dcf079d565b1717c022c045b89c2561659f7dccd
jejwon <110441664+jejwon@users.noreply.github.com>
2022-11-29 12:13:46 +0900

    Jjw (#8)
    * 11/28 지도api
    
    localhost->나중에 주소 변경
    input 위치 수정
    
    * 11/29 getMethod 수정

--- src/main/java/com/footsalhaja/controller/main/MainController.java
+++ src/main/java/com/footsalhaja/controller/main/MainController.java
@@ -1,14 +1,25 @@
 package com.footsalhaja.controller.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.footsalhaja.service.main.MainServiceImpl;
 
 @Controller
 @RequestMapping("main")
 public class MainController {
+	
+	@Autowired
+	private MainServiceImpl service;
+	
 	@GetMapping("get")
-	public void get() {
+	public void get(@RequestParam(name="id") int id,
+			Model model) {
+		service.get(id);
 		
 	}
 	
--- src/main/java/com/footsalhaja/service/main/MainServiceImpl.java
+++ src/main/java/com/footsalhaja/service/main/MainServiceImpl.java
@@ -1,8 +1,18 @@
 package com.footsalhaja.service.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.footsalhaja.mapper.main.MainMapper;
 
 @Service
 public class MainServiceImpl implements MainService {
+
+	@Autowired
+	private MainMapper mapper;
+	public int get(int id) {
+		mapper.insert(id);
+		
+	}
 	// mapper override
 }
